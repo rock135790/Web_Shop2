99a394914c245f92f6123b9da1ce598cd0d19309
rock135790 <152919327+rock135790@users.noreply.github.com>
2025-06-13 21:49:19 +0200

    Initial commit

---
+++ data/mydata.js
@@ -0,0 +1,256 @@
+const data = {
+        website: "Sportska oprema",
+        categories: [
+          {
+            name: "Penjanje",
+            products: [
+              {
+                name: "Penjačice",
+                image: "penjacice.jpg",
+              },
+              {
+                name: "Konop",
+                image: "konop.jpg",
+              },
+              {
+                name: "Magnezij",
+                image: "magnezij.jpg",
+              },
+              {
+                name: "Vrećica za magnezij",
+                image: "vrecica_za_magnezij.jpg",
+              },
+              {
+                name: "Ukapčanja",
+                image: "ukapcanja.jpg",
+              },
+            ],
+          },
+          {
+            name: "Planinarenje",
+            products: [
+              {
+                name: "Gojzerice",
+                image: "gojzerice.jpg",
+              },
+              {
+                name: "Karta",
+                image: "karta.jpg",
+              },
+              {
+                name: "Ruksak",
+                image: "ruksak.jpg",
+              },
+              {
+                name: "Šešir protiv sunca",
+                image: "sesir_protiv_sunca.jpg",
+              },
+              {
+                name: "Kompas",
+                image: "kompas.jpg",
+              },
+            ],
+          },
+          {
+            name: "Plivanje",
+            products: [
+              {
+                name: "Naočale za plivanje",
+                image: "naocale_za_plivanje.jpg",
+              },
+              {
+                name: "Plivačka kapa",
+                image: "plivacka_kapa.jpg",
+              },
+              {
+                name: "Peraje",
+                image: "peraje.jpg",
+              },
+              {
+                name: "Plivaći kostim",
+                image: "plivaci_kostim.jpg",
+              },
+              {
+                name: "Maska i disalica",
+                image: "maska_i_disalica.jpg",
+              },
+            ],
+          },
+          {
+            name: "Dizanje utega",
+            products: [
+              {
+                name: "Girja",
+                image: "girja.jpg",
+              },
+              {
+                name: "Šipka za utege",
+                image: "sipka_za_utege.jpg",
+              },
+              {
+                name: "Bučice",
+                image: "bucice.jpg",
+              },
+              {
+                name: "Elastična traka",
+                image: "elasticna_traka.jpg",
+              },
+              {
+                name: "Medicinka",
+                image: "medicinka.jpg",
+              },
+            ],
+          },
+          {
+            name: "Veslanje",
+            products: [
+              {
+                name: "Skif",
+                image: "skif.jpg",
+              },
+              {
+                name: "Vesla",
+                image: "vesla.jpg",
+              },
+              {
+                name: "Osmerac",
+                image: "osmerac.jpg",
+              },
+              {
+                name: "Ergometar",
+                image: "ergometar.jpg",
+              },
+              {
+                name: "Ležište za veslo",
+                image: "leziste_za_veslo.jpg",
+              },
+            ],
+          },
+          {
+            name: "Tenis",
+            products: [
+              {
+                name: "Reket",
+                image: "reket.jpg",
+              },
+              {
+                name: "Loptice",
+                image: "loptice.jpg",
+              },
+              {
+                name: "Patike",
+                image: "patike.jpg",
+              },
+              {
+                name: "Kapa",
+                image: "kapa.jpg",
+              },
+              {
+                name: "Znojnik",
+                image: "znojnik.jpg",
+              },
+            ],
+          },
+          {
+            name: "Skijanje",
+            products: [
+              {
+                name: "Skije",
+                image: "skije.jpg",
+              },
+              {
+                name: "Štapovi",
+                image: "stapovi.jpg",
+              },
+              {
+                name: "Pancerice",
+                image: "pancerice.jpg",
+              },
+              {
+                name: "Kaciga",
+                image: "kaciga.jpg",
+              },
+              {
+                name: "Rukavice",
+                image: "rukavice.jpg",
+              },
+            ],
+          },
+          {
+            name: "Biciklizam",
+            products: [
+              {
+                name: "Cestovni bicikl",
+                image: "cestovni_bicikl.jpg",
+              },
+              {
+                name: "Brdski bicikl",
+                image: "brdski_bicikl.jpg",
+              },
+              {
+                name: "Biciklističke patike ",
+                image: "biciklisticke_patike.jpg",
+              },
+              {
+                name: "Boca za vodu",
+                image: "boca_za_vodu.jpg",
+              },
+              {
+                name: "Pumpa za gume",
+                image: "pumpa_za_gume.jpg",
+              },
+            ],
+          },
+          {
+            name: "Kanjoning",
+            products: [
+              {
+                name: "Vodonepropusni ruksak",
+                image: "vodonepropusni_ruksak.jpg",
+              },
+              {
+                name: "Spravica za osiguravanje",
+                image: "spravica_za_osiguravanje.jpg",
+              },
+              {
+                name: "Patike za kanjoning",
+                image: "patike_za_kanjoning.jpg",
+              },
+              {
+                name: "Prsluk",
+                image: "prsluk.jpg",
+              },
+              {
+                name: "Pojas",
+                image: "pojas.jpg",
+              },
+            ],
+          },
+          {
+            name: "Surfanje",
+            products: [
+              {
+                name: "Neopren odijelo",
+                image: "neopren_odijelo.jpg",
+              },
+              {
+                name: "Daska za surfanje",
+                image: "daska_za_surfanje.jpg",
+              },
+              {
+                name: "Jedro za windsurf",
+                image: "jedro_za_windsurf.jpg",
+              },
+              {
+                name: "Daska za kitesurf",
+                image: "daska_za_kitesurf.jpg",
+              },
+              {
+                name: "Zmaj za kitesurf",
+                image: "zmaj_za_kitesurf.jpg",
+              },
+            ],
+          },
+        ],
+      };
+      
